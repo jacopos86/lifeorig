34edee5fa634248c2fbe90188dadf63e2b6b4c81
jacopos86 <simonij@tcd.ie>
2026-05-31 00:48:12 -0500

    build chemical network structure -> set workflow

--- src/chem_network/build_network.py
+++ src/chem_network/build_network.py
@@ -1,20 +1,38 @@
 import logging
-from src.chem_network.reaction_network import ReactionNetwork
-from src.input.read_input import p
+from src.input_data.read_input import p
 from src.utilities.logging_module import log
 from src.catalysts.catalysts_set import set_catalysts_prob_distr
+
 #
 # ACF set builder :
-# make a sequence of catalysts set
-# size = size_ACFS
-def build_chem_networks(protocell_list, X_set, size_bpol, catalyst_set_params, rates_params):
+# make a sequence of chem. networks for each protocell
+#
+
+def build_chem_networks(simul_env, X_set, Y_set, metabolites_params, rates_params):
+    '''
+    Build set of chemical networks for each protocell
+    '''
+    # set initial metabolites distribution
+    metabol_distr = simul_env.protocell_list.set_initial_metabolite_distr(X_set, metabolites_params)
     # iterate over protocells
-    for protocell in protocell_list:
-        print(protocell)
-        Rnet = ReactionNetwork(size_bpol)
-        # set up catalyst set
+    for protocell in simul_env.protocell_list:
+        # --------------------------------------
+        # 1)  set up the reaction network
+        # --------------------------------------
+        protocell.set_reaction_network(metabolites_params, X_set)
+        # --------------------------------------
+        # 2) define metabolites popul.
+        # --------------------------------------
+        protocell.set_initial_metabolite_population(metabolites_params, X_set, metabol_distr)
+        protocell.set_initial_protocell_state()
+        # --------------------------------------
+        # 3)  set up catalytic state of set
+        # --------------------------------------
+        protocell.set_catalytic_state(X_set, Y_set)
+        exit()
         catalyst_rr_set = build_catalysts_rr_set(size_C, size_C_intern[i-1])
         catalyst_prob_distr = set_catalysts_prob_distr(size_C, size_C_intern[i-1], ratio_C_ACFset[i-1])
+        protocell.info()
         exit()
         ACFS.set_binary_polymer_model(catalyst_set)
         ACFS.find_ACF_subset()
