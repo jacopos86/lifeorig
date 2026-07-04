1413b85d5c8fc5dc242f496e85da0c5a9b504247
jacopos86 <simonij@tcd.ie>
2026-07-03 19:49:32 -0500

    include surface pind env

--- src/environment/setup_environment.py
+++ src/environment/setup_environment.py
@@ -1,5 +1,6 @@
 from src.environment.volcanic_rocks.volcanic_rock_env import VolcanicRock
 from src.environment.hydro_env.hydrothermal_env import HydrothermalVent
+from src.environment.surf_pond.surface_pond_env import SurfacePond
 
 #
 #   set up environment object
@@ -9,6 +10,8 @@ def set_simulation_environment(protocell_list, env_type, env_data):
     # env_type = hydro_vent | volcanic_rock
     if env_type == "hydro_vent":
         return HydrothermalVent(protocell_list, env_data)
+    elif env_type == "surface_pond":
+        return SurfacePond(protocell_list, env_data)
     elif env_type == "volcanic_rock":
         return VolcanicRock(protocell_list, env_data)
     elif env_type == "Titan":
