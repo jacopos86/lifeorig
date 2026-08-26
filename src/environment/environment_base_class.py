13bf404c0faaee928abb719364b6a77c3eb7428e
jacopos86 <simonij@tcd.ie>
2026-08-26 10:38:03 -0500

    updated environment base class

--- src/environment/environment_base_class.py
+++ src/environment/environment_base_class.py
@@ -13,11 +13,11 @@ class Environment(ABC):
     - protocell placement
     - transport between protocells and environment
     """
-    def __init__(self, protocell_list):
-        self.protocell_list = protocell_list
+    def __init__(self):
         # global conditions
         self.temperature = None
         self.pressure = None
+        self.g = None
         # external molecule reservoir (optional)
         self.external_field = {}
     # ----------------------------
@@ -31,15 +31,29 @@ class Environment(ABC):
         self.apply_transport(dt)
         self.apply_environmental_effects(dt)
     # ----------------------------
+    # get environment state
+    # ----------------------------
+    def get_environment_state(self):
+        state = self._build_state()
+        return {
+            "time": self.state.time,
+            "liquid_level": self.state.liquid_level,
+            "temperature": self.state.temperature,
+            "pressure": self.state.pressure,
+            "gravity": self.state.gravity,
+            "pH": self.state.pH,
+            "ionic_strength": self.state.ionic_strength,
+            "uv_flux": self.state.uv_flux,
+            "sediment_surface_area": self.state.sediment_surface_area,
+            "surface_catalytic_probability_z": self.surface_catalytic_probability_z,
+            "volume": self.state.volume
+        }
+    # ----------------------------
     # abstract methods
     # ----------------------------
     @abstractmethod
     def update_external_conditions(self, dt):
         raise NotImplementedError("update external conditions")
     @abstractmethod
-    def apply_transport(self, dt):
-        raise NotImplementedError("apply transport")
-    @abstractmethod
-    def apply_environmental_effects(self, dt):
-        raise NotImplementedError("apply environmaental effects")
-
+    def set_spatial_geometry(self, geometry):
+        raise NotImplementedError("set the spatial geometry")
