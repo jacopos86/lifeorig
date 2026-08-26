4ef9f6124328e3a3cdd7c22e6a171eab405db3da
jacopos86 <simonij@tcd.ie>
2026-08-26 10:45:22 -0500

    volcanic rock parser

--- src/input_data/parse_volcanic_rock_env.py
+++ src/input_data/parse_volcanic_rock_env.py
@@ -13,27 +13,27 @@ class VolcanicRockLocalEnvironmentParser(LocalEnvironmentParser):
             log.error("Missing 'environment_data' in input")
         return {
             "num_pores": data.get("number_pores"),
-            "pore_radius": self._parse_quantity_from_dict(
+            "pore_radius": self._quantity(
                 input_dict=data.get("pore_radius"),
                 required_keys=("units", "value"),
                 desc="pore radius"
             ),
-            "pore_height": self._parse_quantity_from_dict(
+            "pore_height": self._quantity(
                 input_dict=data.get("pore_height"),
                 required_keys=("units", "value"),
                 desc="pore height"
             ),
-            "distance_neigh_pores": self._parse_quantity_from_dict(
+            "distance_neigh_pores": self._quantity(
                 input_dict=data.get("distance_neigh_pores"),
                 required_keys=("units", "value"),
                 desc="distance neigh. pores"
             ),
-            "temperature": self._parse_quantity_from_dict(
+            "temperature": self._quantity(
                 input_dict=data.get("temperature"),
                 required_keys=("units", "value"),
                 desc="temperature"
             ),
-            "pressure": self._parse_quantity_from_dict(
+            "pressure": self._quantity(
                 input_dict=data.get("pressure"),
                 required_keys=("units", "value"),
                 desc="pressure"
@@ -46,12 +46,12 @@ class VolcanicRockLocalEnvironmentParser(LocalEnvironmentParser):
         return SolventData(
             name=data.get("name", "H2O"),
             liquid_level_params=self._get_liquid_level_params(env_data),
-            density=self._parse_quantity_from_dict(
+            density=self._quantity(
                 input_dict=data.get("density"),
                 required_keys=("units", "value"),
                 desc="solvent density"
             ),
-            dynamic_viscosity=self._parse_quantity_from_dict(
+            dynamic_viscosity=self._quantity(
                 input_dict=data.get("dynamic_viscosity"),
                 required_keys=("units", "value"),
                 desc="solvent dynamic viscosity"
