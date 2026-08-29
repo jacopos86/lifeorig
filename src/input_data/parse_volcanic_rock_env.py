337e99e76016583a0817c3244590b37e30e0768a
Jacopo Simoni <simonij@tcd.ie>
2026-08-29 14:20:24 +0200

    add liquid level parser

--- src/input_data/parse_volcanic_rock_env.py
+++ src/input_data/parse_volcanic_rock_env.py
@@ -1,6 +1,8 @@
 from src.environment.solvent import SolventData
 from src.input_data.environment_base_parser import LocalEnvironmentParser
 from src.utilities.logging_module import log
+from src.environment.external_drive_params import ExternalDriveParams
+from src.environment.liquid_level_model import LiquidLevelParams
 
 #
 #   Volcanic Rock Local environment parser
@@ -39,13 +41,12 @@ class VolcanicRockLocalEnvironmentParser(LocalEnvironmentParser):
                 desc="pressure"
             ),
             "solvent_data": self._get_solvent_data(data),
+            "liquid_level_params": self._get_liquid_level_params(data)
         }
-
     def _get_solvent_data(self, env_data: dict) -> SolventData:
         data = env_data.get("solvent_data", {})
         return SolventData(
             name=data.get("name", "H2O"),
-            liquid_level_params=self._get_liquid_level_params(env_data),
             density=self._quantity(
                 input_dict=data.get("density"),
                 required_keys=("units", "value"),
@@ -66,6 +67,17 @@ class VolcanicRockLocalEnvironmentParser(LocalEnvironmentParser):
                 else float(data.get("polarity"))
             ),
         )
-
+    def _get_liquid_level_params(self, env_data):
+        data = env_data.get("solvent_data", {}).get("liquid_level_params", {})
+        return LiquidLevelParams(
+            model_type=data.get("type", "constant"),
+            base_level=self._quantity(
+                input_dict=data.get("base_level"),
+                required_keys=("units", "value"),
+                desc="liquid base level"
+            )
+        )
+    def _parse_external_drive_params(self, data: dict, desc: str) -> ExternalDriveParams:
+        return None
     def set_external_env_forces(self):
         return None
