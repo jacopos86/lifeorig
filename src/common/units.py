0867974c68f17467e8d6a63077536354eecc786b
Jacopo Simoni <simonij@tcd.ie>
2026-09-07 13:02:40 +0200

    set up internal units

--- src/common/units.py
+++ src/common/units.py
@@ -2,7 +2,7 @@ import pint
 
 ureg = pint.UnitRegistry()
 ureg.define("AU = astronomical_unit")
-Q_ = ureg.Quantity
+Q_ = ureg.Quantity  # type: ignore[misc]
 
 # Define aliases you use often
 THz = ureg.terahertz
@@ -15,3 +15,28 @@ Ang = ureg.angstrom
 
 # Planck constant
 h   = ureg.planck_constant
+
+internal_units = {
+    "time": "s",
+    "energy": "joule",
+    "pressure": "Pa",
+    "density": "kg / m^3",
+    "number_density": "1 / m^3",
+    "temperature": "K",
+    "length": "m",
+    "gravity": "m / s^2",
+    "mass": "kg",
+    "wavelength": "nanometer",
+    "spectral_radiance": "W / m^3 / steradian",
+    "spectral_flux": "W / m^3"
+}
+
+#
+# return magnitude value
+#
+
+def get_magnitude(value: Q_, units: str, default_value: Q_):
+    if value is None:
+        value = default_value
+    value = value.to(units)
+    return float(value.magnitude)
