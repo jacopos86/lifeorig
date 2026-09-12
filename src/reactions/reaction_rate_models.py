1cf373e6212bcd47a5aa219d300579d91664fe4f
Jacopo Simoni <simonij@tcd.ie>
2026-09-12 15:26:21 -0500

    start introducing reaction rate models

--- src/reactions/reaction_rate_models.py
+++ src/reactions/reaction_rate_models.py
@@ -1,4 +1,20 @@
 
+#
+#     Reaction rates factory
+#
+
+
+def build_rate_model(parsed):
+    key = parsed.catalyst_or_control.lower()
+    # check key
+    if key == "2body":
+        _, values = parsed.rate_template.split(":")
+        A, tfac = map(float, values.split())
+        return TwoBodyRate(
+            A=A,
+            tfac=tfac
+        )
+    log.error(f"Unknown rate model: {key}")
 
 #
 #     Reaction rate models
@@ -16,3 +32,17 @@ def reaction_rate_gas_phase_model(Delta, x0, xgr, sig, E_a, T):
 def reaction_rate_surface_catalyst_model():
     ''' Define reaction rates with surface catalysts '''
     pass
+
+#
+#     Reaction rates -> specific to networks
+#
+
+@dataclass
+class TwoBodyRate:
+    A: float
+    tfac: float
+
+    def __call__(self, temperature):
+        return self.A * np.exp(
+            self.tfac / temperature
+        )
